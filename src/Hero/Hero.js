dcd8072e0bc6dbfdf3b2d7eee70f300a7c9a96e0
Matt C <matthew.critelli@gmail.com>
2020-09-13 11:09:49 -0600

    Initial App and Nav component setup.

---
+++ src/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hero.scss';
+
+function Hero() {
+  return (
+    <nav className="Hero">
+    </nav>
+  );
+}
+
+export default Hero;
---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navigation.scss';
+
+function Navigation() {
+  return (
+    <nav className="Navigation">
+      <div className="Navigation__brand">Matt Critelli</div>
+      <ul className="Navigation__list">
+        <li className="Navigation__item">Home</li>
+        <li className="Navigation__item">Skills</li>
+        <li className="Navigation__item">Projects</li>
+        <li className="Navigation__item">Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
