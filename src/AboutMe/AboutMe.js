70a15ddd04c27f0012638d1de0d5fc0a94da3e19
Matt C <matthew.critelli@gmail.com>
2020-10-22 09:52:06 -0600

    Clean up some unused properties. Re-format About Me text to break up chunks of text.

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -18,10 +18,16 @@ function AboutMe() {
               I'm an experienced front-end developer with a passion for working in the space where people and code intersect. The main technologies I use everyday are JavaScript, React, Redux, HTML5, and CSS3. While my most recent experience is focused on these specific client-side technologies, I am excited for opportunities where I can continue to grow on the front-end or broaden my skill-set across the stack.
             </p>
             <p className="AboutMe__text">
-              Prior to my life as a software developer, I worked as a media planner at some of the top ad agencies in New York City. During that time I developed a lot of skills which have resulted in me being a better developer today. As a manager, I was responsible for setting priorities and deadlines and maintaining constant communication with our internal and external teams and departments. Being the primary client contact at the agency, I needed to understand how to communicate our internal technical decisions and deliberations to non-technical stakeholders. While I thoroughly enjoyed the more technical and analytical side of that career, I felt it was missing the creative component which I find deeply rewarding in software engineering.
+              Prior to my life as a software developer, I worked as a media planner at some of the top ad agencies in New York City. During that time I developed a lot of skills which have resulted in me being a better developer today.
             </p>
             <p className="AboutMe__text">
-              After my time living and working in Manhattan, I decided to trade the Big Apple for the Rocky Mountains. When I'm building something cool, you'll probably find me somewhere outside or flipping through some Lonely Planet books planning my next trip. I love traveling, scuba diving, hiking, snowboarding, and dabbling in foreign languages.
+              As a manager, I was responsible for setting priorities and deadlines and maintaining constant communication with our internal and external teams and departments. Being the primary client contact at the agency, I needed to understand how to communicate our internal technical decisions and deliberations to non-technical stakeholders.
+            </p>
+            <p className="AboutMe__text">
+              While I thoroughly enjoyed the more technical and analytical side of that career, I felt it was missing the creative component which I find deeply rewarding in software engineering.
+            </p>
+            <p className="AboutMe__text">
+              After my time living and working in Manhattan, I decided to trade the Big Apple for the Rocky Mountains. When I'm not building something cool, you can find me somewhere outside or flipping through some Lonely Planet books planning my next trip. I love traveling, scuba diving, hiking, snowboarding, and dabbling in foreign languages.
             </p> 
           </div>
         </div>
