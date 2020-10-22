714b71ca83910d448a5716fbac6bea5bf9af5df6
Matt C <matthew.critelli@gmail.com>
2020-10-22 09:00:28 -0600

    Updates

---
+++ precache-manifest.3b260505765ecaaeb5023704054a4a56.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b672bdd998e94f041b4ca0c9a98b3c58",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5c2e49066518d8f0811f",
+    "url": "/portfolio/static/css/main.88ce3ea2.chunk.css"
+  },
+  {
+    "revision": "bdb188af62586b1dc22f",
+    "url": "/portfolio/static/js/2.045f1b57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.045f1b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2e49066518d8f0811f",
+    "url": "/portfolio/static/js/main.e3139466.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "07e0b7a728546850797a4309b808bd6f",
+    "url": "/portfolio/static/media/api-icon.07e0b7a7.svg"
+  },
+  {
+    "revision": "1c465e8b908f0aecfe822c8a19c7a2ab",
+    "url": "/portfolio/static/media/client-icon.1c465e8b.svg"
+  },
+  {
+    "revision": "0fdf59659d674c6c735287e2834dcc95",
+    "url": "/portfolio/static/media/css-icon.0fdf5965.svg"
+  },
+  {
+    "revision": "16467be478499a05bad362cb430758a2",
+    "url": "/portfolio/static/media/github-icon.16467be4.svg"
+  },
+  {
+    "revision": "89ac23fb446c0f408eccf22e024d7200",
+    "url": "/portfolio/static/media/html-icon.89ac23fb.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/portfolio/static/media/javascript-icon2.61ed8b05.svg"
+  },
+  {
+    "revision": "56157efeab1583ed8a922abba5a78d20",
+    "url": "/portfolio/static/media/matt-photo.56157efe.jpg"
+  },
+  {
+    "revision": "2f0ac8ae2c7e3cf20cf3f88481dbd4d9",
+    "url": "/portfolio/static/media/mongodb-icon.2f0ac8ae.svg"
+  },
+  {
+    "revision": "cf677a9c647ec86d978ebde95d297435",
+    "url": "/portfolio/static/media/node-icon.cf677a9c.svg"
+  },
+  {
+    "revision": "f0793d9f85eb3f0d1c702c14d236ba9e",
+    "url": "/portfolio/static/media/react-icon.f0793d9f.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/portfolio/static/media/redux-icon.85772647.svg"
+  },
+  {
+    "revision": "25a6dd09a931a0e6400129e95139a7ea",
+    "url": "/portfolio/static/media/ruby-rails-icon.25a6dd09.svg"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/portfolio/static/media/sass-icon.470548b4.svg"
+  },
+  {
+    "revision": "1632320ae777f4e1168d2b0bb05a248d",
+    "url": "/portfolio/static/media/team-icon.1632320a.svg"
+  }
+]);
